1eb6a5da8fd02496c243ef02a1dbb2ca6e802eeb
Deepa Rangaraj <deeparaj@Deepas-MBP.hsd1.wa.comcast.net>
2022-02-26 17:48:00 -0800

    adding changes

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,16 +1,14 @@
 package com.example.demo;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController 
 {
-	@RequestMapping("home")
+	@RequestMapping("welcome")
 	public String home()
 	{
-		System.out.println("hi");
-		return "home.jsp";
+		return "welcome.jsp";
 	}
 
 }
